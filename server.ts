563fe7ed6aa07847e7a3a43641b1bfa417880a2e
ghackford <hackford.j@northeastern.edu>
2022-03-29 00:58:30 -0400

    removing port specification for localhost with cors

--- server.ts
+++ server.ts
@@ -38,7 +38,7 @@ mongoose.connect(connectionString);
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost'
 }));
 
 const SECRET = 'process.env.SECRET';
