bb03e9e71688adad5cf2c3e7325a15b5ebdefeaa
ghackford <hackford.j@northeastern.edu>
2022-03-30 19:09:16 -0400

    reverting previous change to test netlify login functionality

--- server.ts
+++ server.ts
@@ -37,7 +37,7 @@ mongoose.connect(connectionString);
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: [process.env.CORS_ORIGIN, 'http://localhost']
+    origin: process.env.CORS_ORIGIN;
 }));
 
 let sess = {
